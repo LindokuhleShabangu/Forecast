4999556e44426664c1c9b4ab428498ddf2bf9c42
LindokuhleS <52346780+LindokuhleShabangu@users.noreply.github.com>
2024-03-18 15:00:39 +0200

    Injected the forecast HTML from JavaScript

--- src/app.js
+++ src/app.js
@@ -9,8 +9,6 @@ function displayWeather(response) {
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
- 
-
   cityElement.innerHTML = response.data.city;
 
   timeElement.innerHTML = formatDate(date);
@@ -19,7 +17,7 @@ function displayWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 
   temperatureElement.innerHTML = Math.round(temperature);
-   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
@@ -55,6 +53,35 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function displayForecast(){
+  let days=["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml=" ";
+
+  days.forEach(function (day){
+    forecastHtml=forecastHtml+
+  `
+<div class="weather-forecast-day">
+              <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-icon">🌤️</div>
+                <div class="weather-forecast-temperatures">
+                  <div class="weather-forecast-temperature">
+                  <strong>18&deg</strong>
+                  </div>
+                    <div class="weather-forecast-temperature">12&deg</div>
+
+              </div>
+            </div>`;
+          });
+
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Johannesburg");
+
+
+displayForecast();
